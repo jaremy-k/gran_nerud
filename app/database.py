e7d55eaf464df70befa179c58b8060ee96db0ce8
jaremy-k <art.glorify@gmail.com>
2025-06-29 17:57:19 +0300

    delete postgres configs

--- app/database.py
+++ app/database.py
@@ -1,10 +1,10 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+# from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 from app.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL)
-async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+# engine = create_async_engine(settings.DATABASE_URL)
+# async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 client_mongo = AsyncIOMotorClient(settings.MONGO_URL)
 database_mongo = client_mongo.jaremybase
