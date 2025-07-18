21690f1d83ea0b5fab429327476eacc1cb837ba4
jaremy-k <art.glorify@gmail.com>
2025-07-18 10:38:39 +0300

    update cookies set

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://webhooktestjaremyapi.loca.lt"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=[""],
